ec3872bb03472be7f0157134659cd11e85c78431
minhh8601 <72635674+minhh8601@users.noreply.github.com>
2020-10-10 10:23:15 +0700

    test

---
+++ TH2/Class2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bai5
+{
+    class MaTran
+    {
+        private int sh, sc;
+        private int[,] a;
+        public void Nhap()
+        {
+            Console.Write("Nhap so hang sh:"); sh = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so cot sc:"); sc = int.Parse(Console.ReadLine());
+            a = new int[sh, sc];
+            Console.WriteLine("Nhap thong tin cho cac phan tu cua ma tran");
+            for (int i = 0; i < sh; ++i)
+                for (int j = 0; j < sc; ++j)
+                {
+                    Console.Write("a[{0},{1}]=", i, j);
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+        }
+        public void Hien()
+        {
+            Console.WriteLine("Cac phan tu cua ma tran la");
+            for (int i = 0; i < sh; ++i)
+            {
+                for (int j = 0; j < sc; ++j)
+                    Console.Write("{0}\t", a[i, j]);
+                Console.WriteLine();
+            }
+        }
+        public MaTran Tong(MaTran t2)
+        {
+            if (this.sh == t2.sh && this.sc == t2.sc)
+            {
+                MaTran t = new MaTran();
+                t.sh = this.sh;
+                t.sc = this.sc;
+                t.a = new int[this.sh, this.sc];
+
+                for (int i = 0; i < t.sh; ++i)
+                    for (int j = 0; j < t.sc; ++j)
+                        t.a[i, j] = this.a[i, j] + t2.a[i, j];
+                return t;
+            }
+            else return null;
+        }
+    }
+
+}
---
+++ TH2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace TH2
+{
+    class Diem
+    {
+        private double x;
+        private double y;
+        public Diem(double ox, double oy)
+        {
+            x = ox;
+            y = oy;
+        }
+        public Diem()
+        {
+            x = y = 0;
+        }
+        public void Nhap()
+        {
+            Console.Write("Nhap toa do cua x=");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Nhap toa do cua y=");
+            y = double.Parse(Console.ReadLine());
+        }
+        public void HienThi()
+        {
+            Console.WriteLine("Toa do la: {0},{1}", x, y);
+        }
+        public double KhoangcCach(Diem d1, Diem d2)
+        {
+            double kc = Math.Sqrt((d1.x - d2.x) * (d1.x - d2.x) + (d1.y - d2.y) * (d1.y - d2.y));
+            return kc;
+        }
+        static void Main(string[] args)
+        {
+            Diem t = new Diem();
+            t.Nhap();
+            t.HienThi();
+            Console.ReadKey();
+        }
+    }
+}
